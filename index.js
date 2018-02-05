8c707c7191a4c8d0138e91364570ea613417e9d1
dmfenton <dfenton@esri.com>
2018-02-05 08:59:50 -0500

    add support for advanced providers

--- index.js
+++ index.js
@@ -2,38 +2,43 @@ var FeatureServer = require('featureserver')
 
 function Geoservices () {}
 
-Geoservices.prototype.featureServer = function (req, res) {
+Geoservices.routes = require('./routes')
+Geoservices.type = 'output'
+Geoservices.version = require('./package.json').version
+
+Geoservices.prototype.server = function (req, res) {
+  this.execute('getServer', req, res)
+}
+
+Geoservices.prototype.layer = function (req, res) {
+  this.execute('getLayer', req, res)
+}
+
+Geoservices.prototype.layers = function (req, res) {
+  this.execute('getLayers', req, res)
+}
+
+Geoservices.prototype.query = function (req, res) {
+  this.execute('pull', req, res)
+}
+
+Geoservices.prototype.generateRenderer = function (req, res) {
+  this.execute('generateRenderer', req, res)
+}
+
+Geoservices.prototype.execute = function (action, req, res) {
   // model will be available when this is instantiated with the Koop controller
-  this.model.pull(req, function (err, data) {
+  const handler = this.selectHandler(action, this.model)
+  handler(req, function (err, data) {
     if (err) res.status(err.code || 500).json({error: err.message})
     else FeatureServer.route(req, res, data)
   })
 }
 
-Geoservices.routes = [
-  {
-    path: 'FeatureServer/:layer/:method',
-    methods: ['get', 'post'],
-    handler: 'featureServer'
-  },
-  {
-    path: 'FeatureServer/layers',
-    methods: ['get', 'post'],
-    handler: 'featureServer'
-  },
-  {
-    path: 'FeatureServer/:layer',
-    methods: ['get', 'post'],
-    handler: 'featureServer'
-  },
-  {
-    path: 'FeatureServer',
-    methods: ['get', 'post'],
-    handler: 'featureServer'
-  }
-]
-
-Geoservices.type = 'output'
-Geoservices.version = require('./package.json').version
+Geoservices.prototype.selectHandler = function (model, action) {
+  const prop = this.model[action]
+  if (typeof prop === 'function') return this.model[prop].bind(this.model)
+  else return this.model.pull.bind(this.model)
+}
 
 module.exports = Geoservices
